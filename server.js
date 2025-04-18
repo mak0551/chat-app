74bccc5c88f0f167a125dbb01e5c0dce9b1ae4dc
mohammed ahmed khan <84978114+mak0551@users.noreply.github.com>
2025-04-18 19:27:17 +0530

    code moditfied

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from "express";
-import socketIo from "socket.io";
+import http from "http";
+import { Server } from "socket.io"; // taking server from socket.io, This class lets you create a WebSocket server on top of your HTTP server.
 
 const app = express();
-const io = socketIo(server);
+const server = http.createServer(app); // creating a http server here, Required to manually create an HTTP server, which is needed for attaching Socket.IO.
+
+const io = new Server(server); // io is your Socket.IO server instance. It listens for real-time WebSocket connections from the frontend.
 
 app.use(express.static("public")); // serve static files from public folder
 
 io.on("connection", (socket) => {
-  // io.on('connection'): Listens for a new client connecting via WebSocket. socket: Represents that individual user's connection Each connected user has their own socket
+  // io.on('connection'): Fired whenever a client connects (via browser).. socket: Represents that individual user's connection Each connected user has their own socket
   console.log("A user connected");
 
   socket.on("chat message", (msg) => {
@@ -21,6 +24,6 @@ io.on("connection", (socket) => {
   });
 });
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
