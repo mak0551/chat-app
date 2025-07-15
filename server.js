41afc1a2eba4c55bbacc960561a870c26f0f3897
mohammed ahmed khan <84978114+mak0551@users.noreply.github.com>
2025-07-15 20:08:09 +0530

    users thing added use connected disconneccted alert, add user name along with the message, showing the user name below the text box

--- server.js
+++ server.js
@@ -5,22 +5,43 @@ import { Server } from "socket.io"; // taking server from socket.io, This class 
 const app = express();
 const server = http.createServer(app); // creating a http server here, Required to manually create an HTTP server, which is needed for attaching Socket.IO.
 
-const io = new Server(server); // io is your Socket.IO server instance. It listens for real-time WebSocket connections from the frontend.
+const io = new Server(server); // 'io' is your Socket.IO server instance. It listens for real-time WebSocket connections from the frontend.
 
 app.use(express.static("public")); // serve static files from public folder
+
+let userCount = 0;
+const userMap = new Map(); // Map() is a built-in JS function
 
 io.on("connection", (socket) => {
   // io.on('connection'): Fired whenever a client connects (via browser).. socket: Represents that individual user's connection Each connected user has their own socket
   console.log("A user connected");
 
+  userCount++;
+  const userName = `user${userCount}`;
+
+  userMap.set(socket.id, userName); // Store the user's name in a map with the socket ID as the key, the socket.id is a unique identifier for each connected user.
+
+  socket.broadcast.emit("user connected", `${userName} has joined the chat`); // socket.broadcast.emit(...): Sends a message to all clients except the sender
+
+  socket.emit("your name", userName); // This sends the username **only to the connected client**
+
   socket.on("chat message", (msg) => {
     //socket.on('chat message'): Waits for a 'chat message' event from a user.  msg: The message text sent from the client io.emit(...): Sends that message to everyone, including the sender.
-    io.emit("chat message", msg); // broadcast to all clients
+
+    const name = userMap.get(socket.id); // whatever the userName is stored in the map, we get it using the socket.id
+
+    io.emit("chat message", `${name} : ${msg}`); // broadcast to all clients
   });
 
   socket.on("disconnect", () => {
     // disconnect fires when a user closes their browser/tab or loses connection.
     console.log("User disconnected");
+
+    const name = userMap.get(socket.id); // whatever the userName is stored in the map, we get it using the socket.id
+
+    socket.broadcast.emit("user disconnected", `${name} has left the chat`);
+
+    userMap.delete(socket.id); // Remove the user from the map
   });
 });
 
